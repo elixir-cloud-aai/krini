6e457418c9966961401220a30cf69dfe3a6e9097
Anurag Gupta <anurag@Anurags-MacBook-Air.local>
2022-05-12 12:26:07 +0530

    feat: doc

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,30 @@ const About = () => {
   }, []);
 
   if (!aboutData) {
-    return <div>Loading</div>;
+    return (
+      <div className="pt-28 px-32">
+        <div class="animate-pulse flex space-x-4">
+          <div class="flex-1 space-y-6 py-1">
+            <div class="h-2 bg-slate-300 rounded"></div>
+            <div class="space-y-3">
+              <div class="grid grid-cols-3 gap-4">
+                <div class="h-2 bg-slate-300 rounded col-span-2"></div>
+                <div class="h-2 bg-slate-300 rounded col-span-1"></div>
+              </div>
+              <div class="h-2 bg-slate-300 rounded"></div>
+            </div>
+            <div class="h-2 bg-slate-300 rounded"></div>
+            <div class="space-y-3">
+              <div class="grid grid-cols-3 gap-4">
+                <div class="h-2 bg-slate-300 rounded col-span-2"></div>
+                <div class="h-2 bg-slate-300 rounded col-span-1"></div>
+              </div>
+              <div class="h-2 bg-slate-300 rounded"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
   return (
     <div className="pt-28 px-32">
