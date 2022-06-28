dce626688be4b92a111a31521519c345e53163c2
Anurag Gupta <aguptaking@gmail.com>
2022-06-28 22:37:23 +0530

    feat:open-shift login

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter } from 'react-router-dom';
 import Layout from './components/Layout';
 
 const App = (props) => {
-  console.log(process.env)
   return (
     <BrowserRouter>
       <Layout></Layout>
