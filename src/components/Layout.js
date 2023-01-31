70f77c053137542385ee48064156c9872df8030d
tanya17-05 <tanyakumari332@gmail.com>
2023-01-31 19:22:58 +0530

    feat: cookie consent popup

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,6 +14,7 @@ import Profile from './Profile';
 import toast, { Toaster } from 'react-hot-toast';
 import ManageWorkflows from './ManageWorkflows';
 import Workflow from './Workflow';
+import CookieConsent from './CookieConsent';
 
 const Layout = () => {
   const [scroll, setScroll] = useState(0);
@@ -165,6 +166,7 @@ const Layout = () => {
         <footer>
           <Footer></Footer>
         </footer>
+      <CookieConsent />
       </div>
     </>
   );
