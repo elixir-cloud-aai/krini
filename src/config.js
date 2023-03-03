56e8e8387b0f29c9ac8bc7a6b7e9f1da5e44a729
Anurag Gupta <anurag@Anurags-MacBook-Pro.local>
2023-03-03 20:42:47 +0530

    oidc-login-error

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 const elixirBackend = 'https://stormy-hamlet-10250.herokuapp.com';
-const host_uri = 'https://krini.rahtiapp.fi/';
-// const host_uri = "http://localhost:3000/";
+const host_uri = process.env.REACT_APP_HOST_URI;
 // const host_uri_wes = "https://csc-wes.rahtiapp.fi/ga4gh/wes/v1";
 // const host_uri_wes = "https://weskit.hypatia-comp.athenarc.gr/ga4gh/wes/v1";
 // const host_uri_wes = "https://weskit-test.rahtiapp.fi/ga4gh/wes/v1";
