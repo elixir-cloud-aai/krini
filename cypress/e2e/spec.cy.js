3509a2976de2459c630d7764aa7c08e18521450c
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2023-03-04 11:58:02 +0530

    test: added OIDC auth test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,13 @@
-describe('auth', () => {
-  it('user can login', () => {
-    cy.visit("/")
-    // cy.findByText(/login/i).click()
-  })
-})
+describe("auth", () => {
+  it("user can login", () => {
+    cy.visit("/");
+    cy.contains("Login").click();
+    cy.origin("https://ds.aai.lifescience-ri.eu", () => {
+      cy.contains("Login with LS username").click();
+      cy.get("#username").type("test"); // Some test username
+      cy.get("#password").type("test"); // Some test password
+      cy.get('button[type="submit"]').click();
+    });
+    // cy.contains("Run workflows");
+  });
+});
