b03690c19632a9a202081f6a5b1a63a9a6b3b4cc
Jae Aeich <jh4official@gmail.com>
2023-11-01 18:38:57 +0530

    chore: change the dep

--- src/components/TaskRuns.js
+++ src/components/TaskRuns.js
@@ -1,15 +1,10 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {TESRuns} from 'ecc-client-ga4gh-tes';
-import { 
-    provideFASTDesignSystem, 
-} from '@microsoft/fast-components';
+import { TESRuns } from 'jae-tes';
+import { provideFASTDesignSystem } from '@microsoft/fast-components';
 import { provideReactWrapper } from '@microsoft/fast-react-wrapper';
 
-const { wrap } = provideReactWrapper(
-    React, 
-    provideFASTDesignSystem()
-);
+const { wrap } = provideReactWrapper(React, provideFASTDesignSystem());
 
 const TESRunsComponent = wrap(TESRuns);
 
@@ -20,14 +15,16 @@ const TaskRuns = ({ isLoggedIn, showToast }) => {
     if (isLoggedIn === 'false') {
       return navigate('/');
     }
-  }, [isLoggedIn]); 
+  }, [isLoggedIn]);
 
   return (
-    <div className='tesContainer mt-28 p-1'>
-        <TESRunsComponent className='w-full' baseURL="https://csc-tesk-noauth.rahtiapp.fi/v1"/>
+    <div className="tesContainer mt-28 p-1">
+      <TESRunsComponent
+        className="w-full"
+        baseURL="https://csc-tesk-noauth.rahtiapp.fi/v1"
+      />
     </div>
   );
-
-}
+};
 
 export default TaskRuns;
