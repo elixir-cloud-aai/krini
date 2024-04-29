ffc2e368d193b3f1c96d3041f80a2af46e9d1005
Anurag Gupta <me@anuragxd.com>
2024-04-29 12:40:11 +0530

    fix: tes

--- src/components/TaskCreate.js
+++ src/components/TaskCreate.js
@@ -1,12 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { TESCreateRun } from '@elixir-cloud/tes';
-import { provideFASTDesignSystem } from '@microsoft/fast-components';
-import { provideReactWrapper } from '@microsoft/fast-react-wrapper';
-
-const { wrap } = provideReactWrapper(React, provideFASTDesignSystem());
-
-const TESCreateRunsComponent = wrap(TESCreateRun);
+import ECCClientGa4ghTesCreateRun from '@elixir-cloud/tes/dist/react/create-run';
 
 const TaskCreateRuns = ({ isLoggedIn, showToast }) => {
   const navigate = useNavigate();
@@ -19,10 +13,8 @@ const TaskCreateRuns = ({ isLoggedIn, showToast }) => {
 
   return (
     <div className="tesContainer mt-28 p-1">
-      <TESCreateRunsComponent
-        className="w-full"
-        baseURL="https://protes.rahtiapp.fi/ga4gh/tes/v1"
-      />
+      <ECCClientGa4ghTesCreateRun
+      />;
     </div>
   );
 };
--- src/components/TaskRuns.js
+++ src/components/TaskRuns.js
@@ -1,12 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { TESRuns } from '@elixir-cloud/tes';
-import { provideFASTDesignSystem } from '@microsoft/fast-components';
-import { provideReactWrapper } from '@microsoft/fast-react-wrapper';
-
-const { wrap } = provideReactWrapper(React, provideFASTDesignSystem());
-
-const TESRunsComponent = wrap(TESRuns);
+import ECCClientGa4ghTesRuns from '@elixir-cloud/tes/dist/react/runs';
 
 const TaskRuns = ({ isLoggedIn, showToast }) => {
   const navigate = useNavigate();
@@ -19,9 +13,8 @@ const TaskRuns = ({ isLoggedIn, showToast }) => {
 
   return (
     <div className="tesContainer mt-28 p-1">
-      <TESRunsComponent
-        className="w-full"
-        baseURL="https://protes.rahtiapp.fi/ga4gh/tes/v1"
+      <ECCClientGa4ghTesRuns
+        pageSize={10}
       />
     </div>
   );
